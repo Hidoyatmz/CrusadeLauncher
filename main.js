13707e8ee6350429106962b035736aa9059cc67e
hidoyatmz <hidoyatmz@gmail.com>
2019-07-16 13:27:01 +0200

    Version 1.0.7

--- main.js
+++ main.js
@@ -75,17 +75,14 @@ if (isDev) {
 	console.log('Running in production');
 }
 
-require('update-electron-app')({
-  repo: 'Hidoyatmz/CrusadeLauncher',
-  updateInterval: '5 minutes'
-})
-
 console.log("CHECK UPDATE")
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
+
+require('update-electron-app')()
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
